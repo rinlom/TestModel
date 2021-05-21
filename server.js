484fb885b6cd78f7db9f1f0eab59c0d9727143d2
Jiramate <ta.rinlom@gmail.com>
2021-05-21 20:43:18 +0700

    fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const books = require('./data')
 app.get('/api', (req, res) => {
   res.json(books)
 })
-app.post('/books', (req, res) => {
+app.post('/api', (req, res) => {
   books.push(req.body)
   res.status(201).json(req.body)
 })
